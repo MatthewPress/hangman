80966a4b88d9352204ff3d40a430b9fde5b36683
MatthewPress <press.matt14@gmail.com>
2022-06-21 11:58:09 -0600

    Add letters loop

--- index.js
+++ index.js
@@ -1,30 +1,72 @@
 // Allow user input
 const prompt = require('prompt-sync')();
+var guessedLetter = "";
 var guessedWord = "";
 
 var bodyParts = ["Head", "Body", "Left Arm", "Right Arm", "Left Leg", "Right Leg"];
+var numberGuesses = bodyParts.length;
 
 var words = ["Jazz", "Books", "Window", "Picture", "Lamp"];
 var randomIndex = Math.floor(Math.random() * words.length);
 
+// Testing: Outputs word that needs to be guessed
 console.log(randomIndex, words[randomIndex]);
-console.log(`Your word is ${words[randomIndex].length} characters long`);
 
-do {
-  guessedWord = prompt (`What character would you like to guess?`);
-  if (guessedWord === words[randomIndex]) {
-    console.log(words[randomIndex]);
+function getResponse() {
+  guessedWord = prompt (`Would you like to guess the word or a letter?`);
+  console.log(`Your word is ${words[randomIndex].length} characters long`);
+
+  if (guessedWord === "word" || guessedWord === "Word") {
+    guessedWord = prompt (`What is your guess?`)
+    do {
+      if (guessedWord === words[randomIndex]) {
+        console.log(words[randomIndex]);
+      }
+      else if (guessedWord != words[randomIndex]) {
+        for (var i = 0; i < bodyParts.length; i++) {
+          console.log(bodyParts.pop(bodyParts[i]));
+          numberGuesses -= 1;
+          console.log(`You have ${numberGuesses} guesses left`);
+          guessedWord = prompt (`Incorrect. What character would you like to guess?`);
+        }
+      }
+    } while (bodyParts.length > 1);
   }
-  else if (guessedWord != words[randomIndex]) {
-    for (var i = 0; i < bodyParts.length; i++) {
-      console.log(bodyParts.pop(bodyParts[i]));
-      guessedWord = prompt (`Incorrect. What character would you like to guess?`);
-      break;
-    }
+
+  else if (guessedWord === "letter" || guessedWord === "Letter") {
+    do {
+      guessedLetter = prompt (`What is your guess?`);
+      for (var char = 0; char < words[randomIndex].length; char++) {
+        if (words[randomIndex].charAt(char) === guessedWord) {
+          guessedLetter += word[randomIndex];
+          numberGuesses -= 1;
+          break;
+          // console.log(`${numberGuesses} guesses left`);
+        }
+        else {
+          for (var i = 0; i < bodyParts.length; i++) {
+            console.log(bodyParts.pop(bodyParts[i]));
+            numberGuesses -= 1;
+            console.log(`You have ${numberGuesses} guesses left`);
+            guessedWord = prompt (`Incorrect. What character would you like to guess?`);
+          }
+        }
+      console.log(`${numberGuesses} guesses left`);
+      }
+    } while (word[randomIndex] != guessedLetter || numberGuesses > 1);
   }
-} while (guessedWord != words[randomIndex]);
+  else {
+    //
+  }
+}
+
+getResponse();
 
 /*
+for (var char = 0; char < words[randomIndex].length; char++) {
+  if (words[randomIndex].charAt(char) === guessedWord) {}
+}
+
 if (guessedWord === words[randomIndex]) {
   console.log(words[randomIndex]);
 }
