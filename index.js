503dd1d48670fe9cd9384c5c38eedde8b8fa6415
MatthewPress <press.matt14@gmail.com>
2022-06-22 00:40:14 -0600

    Add comments syntax overhaul

--- index.js
+++ index.js
@@ -1,81 +1,96 @@
-// Allow user input
+// Allow User input
 const prompt = require('prompt-sync')();
+
+// Create an array of words that can randomly be chosen and used to compare guesses against
+var words = ["jazz", "books", "window", "picture", "lamp"];
+var randomIndex = Math.floor(Math.random() * words.length);
+
+// For Testing Purposes Only: Outputs word that needs to be guessed
+// Comment out when done testing
+console.log(randomIndex, words[randomIndex]);
+
+// Create a full list of Body Parts that can be used to limit the number of guesses
+var bodyParts = ["Right Leg", "Left Leg", "Right Arm", "Left Arm", "Body", "Head"];
+var hungBodyParts = [];
+var numberGuesses = bodyParts.length;
+
+// Create arrays that tracks correctly and incorrectly guessed letters
+var alphaGuessedCorrect = [];
+var alphaGuessedIncorrect = [];
+
+// Create variables for prompts
+var wordOrLetter = "";
 var guessedLetter = "";
 var guessedWord = "";
 
-var bodyParts = ["Head", "Body", "Left Arm", "Right Arm", "Left Leg", "Right Leg"];
-var numberGuesses = bodyParts.length;
-
-var words = ["Jazz", "Books", "Window", "Picture", "Lamp"];
-var randomIndex = Math.floor(Math.random() * words.length);
-
-// Testing: Outputs word that needs to be guessed
-console.log(randomIndex, words[randomIndex]);
-
 function getResponse() {
-  guessedWord = prompt (`Would you like to guess the word or a letter?`);
   console.log(`Your word is ${words[randomIndex].length} characters long`);
 
-  if (guessedWord === "word" || guessedWord === "Word") {
-    guessedWord = prompt (`What is your guess?`)
-    do {
+  // Loop: If the correct word is not guessed, the user is asked if they want to guess
+  // a word or letter
+  while (bodyParts.length > 0 && guessedWord != words[randomIndex]) {
+    console.log(`Hung Body Parts: ${hungBodyParts}`);
+    console.log(`Correct Letters Guesses: ${alphaGuessedCorrect}`);
+    console.log(`Incorrect Letters Guesses: ${alphaGuessedIncorrect}`);
+    wordOrLetter = prompt (`Would you like to guess the word or a letter? `);
+
+    // If User wants to guess the word
+    if (wordOrLetter === "word") {
+      guessedWord = prompt (`What word would you like to guess? `)
+
+      // Word is guessed correctly
+      // Congrats messaged is outputed and program closes
       if (guessedWord === words[randomIndex]) {
-        console.log(words[randomIndex]);
+        console.log(`Congrats! ${words[randomIndex]} was the word!`);
       }
+
+      // Word is guessed incorrectly
+      // Last word from the bodyParts array is removed and put in hungBodyParts
+      // numberGuesses left decreases by 1
       else if (guessedWord != words[randomIndex]) {
-        for (var i = 0; i < bodyParts.length; i++) {
-          console.log(bodyParts.pop(bodyParts[i]));
-          numberGuesses -= 1;
-          console.log(`You have ${numberGuesses} guesses left`);
-          guessedWord = prompt (`Incorrect. What character would you like to guess?`);
-        }
+        hungBodyParts.push(bodyParts.pop());
+        numberGuesses -= 1;
+        console.log(`Incorrect. You have ${numberGuesses} guesses left`);
       }
-    } while (bodyParts.length > 1);
-  }
+    }
 
-  else if (guessedWord === "letter" || guessedWord === "Letter") {
-    do {
-      guessedLetter = prompt (`What is your guess?`);
+    // If user wants to guess individual letters of the word
+    else if (wordOrLetter === "letter") {
+      guessedLetter = prompt (`What letter would you like to guess? `);
+
+      // Loop: goes through each character of words[randomIndex]
+      // Need something better that can check all letters in words[randomIndex] at same time
       for (var char = 0; char < words[randomIndex].length; char++) {
-        if (words[randomIndex].charAt(char) === guessedWord) {
-          guessedLetter += word[randomIndex];
-          numberGuesses -= 1;
+
+        // Letter guessed correctly
+        // Guessed letter is put in alphaGuessed array
+        // numberGuesses does not decrease
+        if (words[randomIndex].charAt(char) === guessedLetter) {
+          // guessedLetter += word[randomIndex];
+          alphaGuessedCorrect.push(guessedLetter);
+          console.log(`Correct. The word contains ${guessedLetter}.
+          You have ${numberGuesses} guesses left.`);
           break;
-          // console.log(`${numberGuesses} guesses left`);
         }
+
+        // Letter guessed incorrectly
+        // Last word from the bodyParts array is removed and put in hungBodyParts
+        // Guessed letter is put in alphaGuessed array
+        // numberGuesses left decreases by 1
         else {
-          for (var i = 0; i < bodyParts.length; i++) {
-            console.log(bodyParts.pop(bodyParts[i]));
-            numberGuesses -= 1;
-            console.log(`You have ${numberGuesses} guesses left`);
-            guessedWord = prompt (`Incorrect. What character would you like to guess?`);
-          }
+          hungBodyParts.push(bodyParts.pop());
+          alphaGuessedIncorrect.push(guessedLetter);
+          numberGuesses -= 1;
+          console.log(`Incorrect. You have ${numberGuesses} guesses left.`);
+          break;
         }
-      console.log(`${numberGuesses} guesses left`);
       }
-    } while (word[randomIndex] != guessedLetter || numberGuesses > 1);
-  }
-  else {
-    //
-  }
-}
+    }
 
+    else {
+    } // else
+  } // while
+} // function
+
+// Call getResponse function
 getResponse();
-
-/*
-for (var char = 0; char < words[randomIndex].length; char++) {
-  if (words[randomIndex].charAt(char) === guessedWord) {}
-}
-
-if (guessedWord === words[randomIndex]) {
-  console.log(words[randomIndex]);
-}
-
-else if (guessedWord != words[randomIndex]) {
-  while (guessedWord != words[randomIndex]) {
-    guessedWord = prompt (`Incorrect. What character would you like to guess?`);
-  }
-}
-
-else {console.log(words[randomIndex]);}
-*/
