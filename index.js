642f1e835a48815e53f5448652dd79744f037537
Eric Weissman <eric@example.com>
2022-06-28 16:14:07 -0600

    Initial commit

--- index.js
+++ index.js
@@ -1,81 +1,105 @@
-// Allow user input
+// Allow user input z
 const prompt = require('prompt-sync')();
-var guessedLetter = "";
-var guessedWord = "";
+//start of function
+function startGame() {
+  //intialize virables
+  var wordFromUser;
+  var userWord = "";
+  var userLetters = "";
+  var lengthOfletter = 0;
 
-var bodyParts = ["Head", "Body", "Left Arm", "Right Arm", "Left Leg", "Right Leg"];
-var numberGuesses = bodyParts.length;
+  //intialize array for randomwords and body bodyParts for the GAME
+  var wordList = ["Jazz", "Books", "Window", "Picture", "Lamp"];
+  var bodyParts = ["Head", "Body", "Left Arm", "Right Arm", "Left Leg", "Right Leg"];
+  var turnLift = bodyParts.length;
+  //getting a random index
+  var randomIndex = Math.floor(Math.random() * wordList.length);
+  var randomWord = wordList[randomIndex].toUpperCase();
 
-var words = ["Jazz", "Books", "Window", "Picture", "Lamp"];
-var randomIndex = Math.floor(Math.random() * words.length);
 
-// Testing: Outputs word that needs to be guessed
-console.log(randomIndex, words[randomIndex]);
-
-function getResponse() {
-  guessedWord = prompt (`Would you like to guess the word or a letter?`);
-  console.log(`Your word is ${words[randomIndex].length} characters long`);
-
-  if (guessedWord === "word" || guessedWord === "Word") {
-    guessedWord = prompt (`What is your guess?`)
-    do {
-      if (guessedWord === words[randomIndex]) {
-        console.log(words[randomIndex]);
+  wordFromUser = prompt("Do you want to guess a letter or a word? ").toUpperCase();
+  userWord = wordFromUser.toUpperCase();
+  console.log(randomWord);
+  if (userWord === "WORD") {
+    wordFromUser = prompt("What word do you want to guess? ").toUpperCase();
+    userWord = wordFromUser.toUpperCase();
+    //while loops that will run while the user guess the wrong word
+    while (turnLift !== 0) {
+      if (turnLift === 0) {
+        console.log("GAME OVER!!!!");
       }
-      else if (guessedWord != words[randomIndex]) {
-        for (var i = 0; i < bodyParts.length; i++) {
-          console.log(bodyParts.pop(bodyParts[i]));
-          numberGuesses -= 1;
-          console.log(`You have ${numberGuesses} guesses left`);
-          guessedWord = prompt (`Incorrect. What character would you like to guess?`);
+      if (userWord.toUpperCase() !== randomWord.toUpperCase() && turnLift !== 0) {
+        //bodyParts.pop();
+        userWord = wordFromUser.toUpperCase();
+        turnLift--;
+        console.log("you have lost your", bodyParts.pop());
+        console.log("you have", turnLift, "Moves!");
+        //checking if user has 0 number of guess left
+        if (turnLift === 0) {
+          console.log("Game Over!");
         }
+        console.log(`Your Word is ${randomWord.length} letters long`);
+        wordFromUser = prompt("What word do you want to guess? ").toUpperCase();
+        userWord = wordFromUser.toUpperCase();
+      } else if (wordFromUser === randomWord) {
+        userWord = randomWord.toUpperCase();
+        console.log("YOU WIN YOU HAVE GUESSED THE RIGHT WORD");
+        break;
       }
-    } while (bodyParts.length > 1);
-  }
-
-  else if (guessedWord === "letter" || guessedWord === "Letter") {
-    do {
-      guessedLetter = prompt (`What is your guess?`);
-      for (var char = 0; char < words[randomIndex].length; char++) {
-        if (words[randomIndex].charAt(char) === guessedWord) {
-          guessedLetter += word[randomIndex];
-          numberGuesses -= 1;
-          break;
-          // console.log(`${numberGuesses} guesses left`);
-        }
-        else {
-          for (var i = 0; i < bodyParts.length; i++) {
-            console.log(bodyParts.pop(bodyParts[i]));
-            numberGuesses -= 1;
-            console.log(`You have ${numberGuesses} guesses left`);
-            guessedWord = prompt (`Incorrect. What character would you like to guess?`);
+    }
+  } else if (wordFromUser === randomWord) {
+    userWord = randomWord.toUpperCase();
+    console.log("YOU WIN YOU HAVE GUESSED THE RIGHT WORD");
+    //break;
+  } else if (userWord === "LETTER") {
+    //wordFromUser = prompt("What letter do you want to guess? ").toUpperCase();
+    console.log(`Your Word is ${randomWord.length} letters long`);
+    wordFromUser = prompt("What letter do you want to guess? ").toUpperCase();
+    userGuessedChar = wordFromUser.toUpperCase();
+    while (turnLift !== 0) {
+      if (userGuessedChar.length === 1) {
+        if (randomWord.includes(userGuessedChar) === false && turnLift !== 0) {
+          turnLift--;
+          console.log("you have", turnLift, "Moves!");
+          console.log("you have lost your", bodyParts.pop());
+          console.log("try again");
+          wordFromUser = prompt("What letter do you want to guess? ").toUpperCase();
+          userGuessedChar = wordFromUser.toUpperCase();
+          //wordFromUser = prompt("Do you want to guess word or letter? ").toUpperCase();
+          userGuessedChar = wordFromUser.toUpperCase();
+        } else if (randomWord.includes(userGuessedChar) && userGuessedChar.length === 1) {
+          //wordFromUser = prompt("What word do you want to guess? ").toUpperCase();
+          userLetters += userGuessedChar;
+          lengthOfletter++;
+          console.log("the right letters are: ", userLetters);
+          if (randomWord.length === lengthOfletter) {
+            console.log(`you have guessed the word ${randomWord}`);
+            console.log("You win");
+            break;
           }
+          wordFromUser = prompt("What letter do you want to guess? ").toUpperCase();
+          userGuessedChar = wordFromUser.toUpperCase();
+          //letting the user know how much more moves/guesses they have lift
+        } else if (turn === 1) {
+          console.log("lost guess");
+          turnLift--;
+        } else if (turnLift === 0) {
+          console.log("Game OVER");
         }
-      console.log(`${numberGuesses} guesses left`);
+        //checking if the characters are in the in the random string
+      } else if (randomWord.includes(userGuessedChar) && userGuessedChar.length === 1) {
+        console.log("the right letters are: ", userLetters);
+        if (randomWord.length === lengthOfletter) {
+          console.log(`you have guessed the word ${randomWord}`);
+          console.log("You win");
+          //breaking out of while loop if number of current letter are equel to the letter in the randomWord
+          break;
+        }
+        //letting the user know how much more moves/guesses they have lift
       }
-    } while (word[randomIndex] != guessedLetter || numberGuesses > 1);
-  }
-  else {
-    //
+    }
+
   }
 }
 
-getResponse();
-
-/*
-for (var char = 0; char < words[randomIndex].length; char++) {
-  if (words[randomIndex].charAt(char) === guessedWord) {}
-}
-
-if (guessedWord === words[randomIndex]) {
-  console.log(words[randomIndex]);
-}
-
-else if (guessedWord != words[randomIndex]) {
-  while (guessedWord != words[randomIndex]) {
-    guessedWord = prompt (`Incorrect. What character would you like to guess?`);
-  }
-}
-
-else {console.log(words[randomIndex]);}
-*/
+startGame();
